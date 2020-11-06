9aea41dbb0a9dc5fde99ef6217737feba7b2410b
PsychoPuss <evgeniy.v.ilyin@gmail.com>
2020-11-06 21:49:21 +0300

    boxes popup css fixes

--- _source/js/components/carousel-temp.js
+++ _source/js/components/carousel-temp.js
@@ -1,90 +1,91 @@
 /* Carousel init */
 let breakpoints = {
-	xs: 320,
-	sm: 425,
-	md: 768,
-	lg: 1024,
-	xl: 1180
+  xs: 320,
+  sm: 425,
+  md: 768,
+  lg: 1024,
+  xl: 1180,
 
-	//  small: 0,
-	// 	medium: 518,
-	// 	large: 758,
-	// 	xlarge: 998,
-	// 	xxlarge: 1238
-}
+  //  small: 0,
+  // 	medium: 518,
+  // 	large: 758,
+  // 	xlarge: 998,
+  // 	xxlarge: 1238
+};
 
 function initOwl() {
-	let $mainSlider = $('.main-carousel>.owl-carousel');
-	let $whomSlider = $('.for-whom__carousel>.owl-carousel');
-	let $productSlider = $('.product__carousel>.owl-carousel');
-	let $activateSlider = $('.activate__carousel>.owl-carousel');
-	// $mainNews = $('.main-news-carousel'),
-	// $mainBrand = $('.main-brand-carousel'),
-	// $productPreview = $('.product-slider'),
-	// $productPack = $('.product-pack-carousel'),
-	// $productSet = $('.product-set-carousel'),
-	// $productCompare = $('.product-compare-carousel'),
-	// $productSeeIt = $('.product-carousel'),
-	// $innerGallery = $('.inner-carousel'),
-	// $innerTeam = $('.inner-team');
+  let $mainSlider = $(".main-carousel>.owl-carousel");
+  let $whomSlider = $(".for-whom__carousel>.owl-carousel");
+  let $productSlider = $(".product__carousel>.owl-carousel");
+  let $activateSlider = $(".activate__carousel>.owl-carousel");
+  // $mainNews = $('.main-news-carousel'),
+  // $mainBrand = $('.main-brand-carousel'),
+  // $productPreview = $('.product-slider'),
+  // $productPack = $('.product-pack-carousel'),
+  // $productSet = $('.product-set-carousel'),
+  // $productCompare = $('.product-compare-carousel'),
+  // $productSeeIt = $('.product-carousel'),
+  // $innerGallery = $('.inner-carousel'),
+  // $innerTeam = $('.inner-team');
 
-	if ($mainSlider.length) {
-		let itemLength = $mainSlider.find('.main-carousel__item').length,
-			params = {
-				items: 1,
-				autoplay: true,
-				margin: 10,
-				navText: ['пред.', 'след.'],
-			};
-		if (itemLength > 1) {
-			params['loop'] = true;
-			params['nav'] = true;
-		} else {
-			params['loop'] = false;
-			params['dots'] = false;
-		}
-		$mainSlider.owlCarousel(params);
-	}
+  if ($mainSlider.length) {
+    let itemLength = $mainSlider.find(".main-carousel__item").length,
+      params = {
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        margin: 10,
+        navText: ["пред.", "след."],
+      };
+    if (itemLength > 1) {
+      params["loop"] = true;
+      params["nav"] = true;
+    } else {
+      params["loop"] = false;
+      params["dots"] = false;
+    }
+    $mainSlider.owlCarousel(params);
+  }
 
-	if ($whomSlider.length) {
-		let itemLength = $whomSlider.find('.for-whom__carousel-item').length,
-			params = {
-				margin: 40,
-				loop: false,
-				dots: false,
-				autoWidth: true,
-				items: 1,
-				navText: [],
-			};
-		$whomSlider.owlCarousel(params);
-	}
+  if ($whomSlider.length) {
+    let itemLength = $whomSlider.find(".for-whom__carousel-item").length,
+      params = {
+        margin: 40,
+        loop: false,
+        dots: false,
+        autoWidth: true,
+        items: 1,
+        navText: [],
+      };
+    $whomSlider.owlCarousel(params);
+  }
 
-	if ($productSlider.length) {
-		let itemLength = $productSlider.find('.product__carousel-item').length,
-			params = {
-				margin: 0,
-				loop: false,
-				dots: false,
-				autoWidth: true,
-				items: 1,
-				navText: [],
-			};
-		$productSlider.owlCarousel(params);
-	}
+  if ($productSlider.length) {
+    let itemLength = $productSlider.find(".product__carousel-item").length,
+      params = {
+        margin: 0,
+        loop: false,
+        dots: false,
+        autoWidth: true,
+        items: 1,
+        navText: [],
+      };
+    $productSlider.owlCarousel(params);
+  }
 
-	if ($activateSlider.length) {
-		let itemLength = $activateSlider.find('.activate__carousel-item').length,
-			params = {
-				margin: 0,
-				loop: false,
-				dots: false,
-				autoWidth: true,
-				items: 1,
-				navText: [],
-			};
-		$activateSlider.owlCarousel(params);
-	}
-	/*
+  if ($activateSlider.length) {
+    let itemLength = $activateSlider.find(".activate__carousel-item").length,
+      params = {
+        margin: 0,
+        loop: false,
+        dots: false,
+        autoWidth: true,
+        items: 1,
+        navText: [],
+      };
+    $activateSlider.owlCarousel(params);
+  }
+  /*
 		if ($mainBanner.length) {
 			$mainBanner.each(function () {
 				var itemLength = $(this).find('.item').length,
